9b025b0b4b6721a10ae183f8720efdd11c83e1ba
Haylie Tan <141064325+haylietan@users.noreply.github.com>
2024-09-30 13:08:25 -0700

    Fixed errors and removed TableLocations file

--- app/(pages)/(index-page)/_components/JudgingHub/JudgingHub.tsx
+++ app/(pages)/(index-page)/_components/JudgingHub/JudgingHub.tsx
@@ -1,22 +1,18 @@
 import { useAuth } from '@hooks/useAuth';
-
 import styles from './JudgingHub.module.scss';
 import HubHero from './HubHero';
-import JudgingList from './JudgingList';
 import TableLocations from './TableLocations';
 import ViewProjects from './ViewProjects';
-import { useSubmissions } from '@hooks/useSubmissions';
 import { useJudgeGroup } from '@hooks/useJudgeGroup';
 
 export default function JudgingHub() {
   const { user, loading } = useAuth();
   const { members } = useJudgeGroup();
-  const { unjudgedTeams } = useSubmissions();
   return (
     <div className={styles.container}>
-      <ViewProjects/>
+      <ViewProjects />
       <HubHero user={user} loading={loading} members={members} />
-      <JudgingList projects={unjudgedTeams} />
+      {/* <JudgingList projects={unjudgedTeams} /> */}
       <TableLocations />
     </div>
   );
--- app/(pages)/(index-page)/_components/JudgingHub/TableLocations.tsx
+++ app/(pages)/(index-page)/_components/JudgingHub/TableLocations.tsx
@@ -1,12 +1,10 @@
 import styles from './TableLocations.module.scss';
-import LogoutButton from '../LogoutButton/LogoutButton';
-import Link from 'next/link';
 import Image from 'next/image';
 import vinyl from '/public/judges/hub/vinyl.svg';
 import bunnyHand from '/public/judges/hub/bunny-hand.svg';
 import vinylPlayer from '/public/judges/hub/vinyl-player.svg';
 export default function TableLocations() {
-  const logOutStyle = {
+  /* const logOutStyle = {
     zIndex: 1,
     borderRadius: '15.497px',
     background: '#9EE7E5',
@@ -26,36 +24,24 @@ export default function TableLocations() {
   };
   const figmaLink =
     'https://www.figma.com/proto/9frZI5Kc9f2c8o4ZIZG8fX/Judging-Table-Map?page-id=0:1&type=design&node-id=1-4&viewport=134,164,0.69&t=Jfp4HXeR7nRs3B6R-1&scaling=min-zoom&mode=design';
-
+ */
   return (
     <div className={styles.container}>
       <p>
-        <div className={styles.header}>If you have any questions, please  ask a HackDavis director (dark blue shirt)!</div>
+        <div className={styles.header}>
+          If you have any questions, please ask a HackDavis director (dark blue
+          shirt)!
+        </div>
       </p>
-
       <div className={styles.vinyl_container}>
-        <Image
-          src={vinyl}
-          alt="Vinyl"
-          className={styles.vinyl}
-        />
-
-        <Image
-          src={bunnyHand}
-          alt="Bunny hand"
-          className={styles.bunny_hand}
-        />
-
+        <Image src={vinyl} alt="Vinyl" className={styles.vinyl} />
+        <Image src={bunnyHand} alt="Bunny hand" className={styles.bunny_hand} />
         <Image
           src={vinylPlayer}
           alt="Vinyl Player"
           className={styles.vinyl_player}
         />
-      </div>  
-
-      {/* <LogoutButton style={logOutStyle}>
-        <p>Sign out</p>
-      </LogoutButton> */}
+      </div>
     </div>
   );
 }
